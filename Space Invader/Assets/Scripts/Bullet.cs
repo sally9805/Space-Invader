07cbe0d61ce47f843e94799d7e8c19df9cfde5fb
sally9805 <haishangao2020@u.northwestern.edu>
2018-11-10 15:50:56 -0600

    first update

---
+++ Space Invader/Assets/Scripts/Bullet.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+	private Transform bulletTrans;
+	public float speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Space Invader/Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+	private Transform playerTrans;
+	public float speed;
+	public float boundLeft, boundRight;
+
+	// Use this for initialization
+	void Start ()
+	{
+		playerTrans = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		Vector3 pos = playerTrans.position;
+		if (Input.GetKey ("d") || Input.GetKey("right")) pos.x += speed * Time.deltaTime;
+		if (Input.GetKey ("a") || Input.GetKey("left")) pos.x -= speed * Time.deltaTime;
+		playerTrans.position = pos;
+	}
+}
